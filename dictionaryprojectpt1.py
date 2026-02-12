af16300352181ce6b8fed64258429a966eeac9fb
QC2222 <quintonc12@nycstudents.net>
2026-02-12 10:13:22 -0500

    tungstencube

---
+++ dictionaryprojectpt1.py
@@ -0,0 +1,17 @@
+sixinwcube = {
+    "name": "6\" Tungsten Cube",
+    "price": 9689.99,
+    "mass": "65 kilograms, 143.3 pounds"
+}
+fiftyfivegallondrumofapplejuice = {
+    "name": "55 Gallon Drum of Apple Juice",
+    "price": 799.99,
+    "mass": "220 kilograms, 485.02 pounds"
+}
+singlepencil = {
+    "name": "Number #2 HB Pencil",
+    "price": 0.19,
+    "mass": "0.05 kilograms, 0.11 pounds"
+}
+print("Welcome to my humble store! Here are my wares:")
+print(sixinwcube[0])
