2bee0d643fa14f3c460ae5c968472d83fb3b716d
QC2222 <quintonc12@nycstudents.net>
2026-02-13 10:16:24 -0500

    fire products yo

--- dictionaryprojectpt1.py
+++ dictionaryprojectpt1.py
@@ -1,17 +1,33 @@
-sixinwcube = {
+wares = [{
     "name": "6\" Tungsten Cube",
     "price": 9689.99,
-    "mass": "65 kilograms, 143.3 pounds"
-}
-fiftyfivegallondrumofapplejuice = {
+    "mass": "65 kilograms, 143.3 pounds",
+    "stock": 9223372036854775807
+}, 
+{
     "name": "55 Gallon Drum of Apple Juice",
     "price": 799.99,
-    "mass": "220 kilograms, 485.02 pounds"
-}
-singlepencil = {
+    "mass": "220 kilograms, 485.02 pounds",
+    "stock": 340282366920938463463374607431768211456
+
+},
+{
     "name": "Number #2 HB Pencil",
     "price": 0.19,
-    "mass": "0.05 kilograms, 0.11 pounds"
-}
+    "mass": "0.05 kilograms, 0.11 pounds",
+    "stock": [2, "two"]
+},
+{
+    "name": "The Sun",
+    "price": 5,
+    "mass": "1,900,000,000,000,000,000,000,000,000,000 kilograms, 4.188782981512674e+30 pounds",
+    "stock": 1
+},
+{
+    "name": "TON 618",
+    "price": 15.99,
+    "mass": "66 billion solar masses, 2.8939798982e+41",
+    "stock": 3
+}]
 print("Welcome to my humble store! Here are my wares:")
-print(sixinwcube[0])
+print(wares["name"])
